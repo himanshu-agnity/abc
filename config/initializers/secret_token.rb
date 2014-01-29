b07c027797c5d47d6c0e3dff89ed38aa6121b578
Agnity <himanshu.upadhyay@agnity.com>
2014-01-29 12:27:17 +0530

    new files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Temp::Application.config.secret_token = '513412a99f191bac499205e28814ba7fb7473ac027d41df3f2dd63d0e64a84d24427fdef0d8fd13c21483f19a1ae4bfccf155e69075cc5577a659b94358f4a6c'
